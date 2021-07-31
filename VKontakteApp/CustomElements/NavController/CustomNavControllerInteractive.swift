e9e36164594b0655aa7f9a590f95613268e5e783
Chumakova Olga <tresa1@mail.ru>
2021-07-31 14:23:25 +0300

    lesson9

---
+++ VKontakteApp/CustomElements/NavController/CustomNavControllerInteractive.swift
@@ -0,0 +1,44 @@
+//
+//  CustomNavControllerInteractive.swift
+//  VKontakteApp
+//
+//  Created by Olga Chumakova on 31.07.2021.
+//
+
+
+import UIKit
+
+
+final class CustomNavControllerInteractiv: UINavigationController {
+
+    let interactiveTransition = InteractiveTransition()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+}
+
+extension CustomNavControllerInteractiv: UINavigationControllerDelegate {
+
+    func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+
+        return interactiveTransition.isStarted ? interactiveTransition : nil
+    }
+
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        switch operation {
+        case .pop:
+            if navigationController.viewControllers.first != toVC {
+                interactiveTransition.viewController = toVC
+            }
+            return PopAnimator()
+        case .push:
+            interactiveTransition.viewController = toVC
+            return PushAnimator()
+        default:
+            return nil
+        }
+    }
+}
+
